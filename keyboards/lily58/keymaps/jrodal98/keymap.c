a37eefba0d2c0f9f337b4d5db4f1e3f1a4e726a5
Jacob Rodal <jrodal@fb.com>
2021-10-10 02:23:55 -0700

    caps lock on shift + mac lock screen leader

--- keyboards/lily58/keymaps/jrodal98/keymap.c
+++ keyboards/lily58/keymaps/jrodal98/keymap.c
@@ -34,7 +34,7 @@ enum {
 /* }; */
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-	[_QWERTY] = LAYOUT(KC_GRV, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_EQL, KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_MINS, LCTL_T(KC_ESC), KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_LBRC, KC_RBRC, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_LEAD, KC_LALT, KC_LGUI, KC_SPC, MO(_LOWER), KC_ENT, KC_BSPC, MO(_RAISE), KC_MEH),
+	[_QWERTY] = LAYOUT(KC_GRV, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_EQL, KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_MINS, LCTL_T(KC_ESC), KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, LSFT_T(KC_CAPS), KC_Z, KC_X, KC_C, KC_V, KC_B, KC_LBRC, KC_RBRC, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_LEAD, KC_LALT, KC_LGUI, KC_SPC, MO(_LOWER), KC_ENT, KC_BSPC, MO(_RAISE), KC_MEH),
 	[_LOWER] = LAYOUT(KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, KC_TILD, DM_REC1, DM_PLY1, KC_HASH, KC_DLR, KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LBRC, KC_RBRC, KC_PIPE, KC_GRV, KC_UNDS, DUNDER, ARROW, SPACED_EQUAL, KC_PEQL, KC_TRNS, KC_LPRN, KC_RPRN, KC_LCBR, KC_RCBR, KC_PIPE, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_LCBR, KC_RCBR, KC_TRNS, KC_UNDS, KC_PLUS, KC_TRNS, KC_TRNS, KC_BSLS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_DEL, KC_TRNS, KC_TRNS),
 	[_RAISE] = LAYOUT(KC_TRNS, KC_MUTE, KC_VOLD, KC_VOLU, KC_MPRV, KC_MNXT, KC_BRID, KC_BRIU, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_GRV, KC_1, KC_MS_U, KC_3, KC_4, KC_5, KC_HOME, KC_PGDN, KC_PGUP, KC_END, KC_0, KC_TRNS, KC_F1, KC_MS_L, KC_MS_D, KC_MS_R, KC_F5, KC_F6, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, KC_NO, KC_NO, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, KC_TRNS, KC_TRNS, KC_PLUS, KC_MINS, KC_EQL, KC_LBRC, KC_RBRC, KC_BSLS, KC_TRNS, KC_TRNS, KC_BTN1, KC_TRNS, KC_TRNS, KC_BTN2, KC_TRNS, KC_TRNS),
   [_ADJUST] = LAYOUT( \
@@ -129,7 +129,21 @@ void matrix_scan_user(void) {
     leader_end();
 
     // SEQ_ONE_KEY, SEQ_TWO_KEYS, and SEQ_THREE_KEYS are options
+    //
+    //
+    ///////////////////////// MAC //////////////////////////
 
+    // Lock screen
+    SEQ_ONE_KEY(KC_L) {
+      register_code(KC_LGUI);
+      register_code(KC_LCTL);
+      register_code(KC_Q);
+      unregister_code(KC_Q);
+      unregister_code(KC_LCTL);
+      unregister_code(KC_LGUI);
+    }
+
+    //////////////////// FB /////////////////////////////////////
     // D: Diff
     SEQ_TWO_KEYS(KC_D, KC_S) {
       // Anything you can do in a macro.
