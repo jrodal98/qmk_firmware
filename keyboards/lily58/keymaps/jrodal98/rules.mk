7ee4fd4147c9d3a137dd08f97d041735cdbeb8b7
Jacob Rodal <jrodal@fb.com>
2021-10-30 15:20:24 -0700

    updates

--- keyboards/lily58/keymaps/jrodal98/rules.mk
+++ keyboards/lily58/keymaps/jrodal98/rules.mk
@@ -12,7 +12,7 @@ BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
 AUDIO_ENABLE = no           # Audio output
 RGBLIGHT_ENABLE = no       # Enable WS2812 RGB underlight.
 SWAP_HANDS_ENABLE = no      # Enable one-hand typing
-OLED_ENABLE= no     # OLED display, this was the only one that was 'yes' by default
+OLED_ENABLE = yes     # OLED display, this was the only one that was 'yes' by default
 DYNAMIC_MACRO_ENABLE = yes # Added by me, allows recording up to two macros: https://docs.qmk.fm/#/feature_dynamic_macros
 TAP_DANCE_ENABLE = no # added by me, does something special when a key is tapped twice
 KEY_LOCK_ENABLE = no
@@ -21,11 +21,13 @@ LEADER_ENABLE = yes
 # Do not enable SLEEP_LED_ENABLE. it uses the same timer as BACKLIGHT_ENABLE
 SLEEP_LED_ENABLE = no    # Breathing sleep LED during USB suspend
 
+BOOTLOADER = atmel-dfu  # Elite-C
+
 # If you want to change the display of OLED, you need to change here
-SRC +=  ./lib/rgb_state_reader.c \
-        ./lib/layer_state_reader.c \
+SRC +=  ./lib/layer_state_reader.c \
         ./lib/logo_reader.c \
         ./lib/keylogger.c \
+	# ./lib/rgb_state_reader.c \
         # ./lib/mode_icon_reader.c \
         # ./lib/host_led_state_reader.c \
         # ./lib/timelogger.c \
