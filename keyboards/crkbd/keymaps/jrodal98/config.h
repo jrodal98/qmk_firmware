46d87c420790d9042a9b43a3ba5ddec7216ce70f
Jacob Rodal <github@jrodal.com>
2026-02-16 13:42:44 -0800

    improved blok versus elite-c handling

--- keyboards/crkbd/keymaps/jrodal98/config.h
+++ keyboards/crkbd/keymaps/jrodal98/config.h
@@ -1,32 +1,6 @@
-/*
-This is the c configuration file for the keymap
-
-Copyright 2012 Jun Wako <wakojun@gmail.com>
-Copyright 2015 Jack Humbert
-
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 2 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
-
 #pragma once
 
-//#define USE_MATRIX_I2C
-
-/* Select hand configuration */
-
 #define MASTER_LEFT
-// #define MASTER_RIGHT
-// #define EE_HANDS
 
 #define USE_SERIAL_PD2
 
@@ -37,17 +11,12 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 // RGB MATRIX MODES
 // https://docs.qmk.fm/#/feature_rgb_matrix
-#define ENABLE_RGB_MATRIX_BREATHING
+#ifdef RGB_MATRIX_ENABLE
+    #define ENABLE_RGB_MATRIX_BREATHING
+    #define RGB_MATRIX_FRAMEBUFFER_EFFECTS
+    #define ENABLE_RGB_MATRIX_TYPING_HEATMAP
+#endif // RGB_MATRIX_ENABLERGB_MATRIX_ENABLE
 
-#define RGB_MATRIX_FRAMEBUFFER_EFFECTS
-#define ENABLE_RGB_MATRIX_TYPING_HEATMAP
-
-#ifdef RGBLIGHT_ENABLE
-    #define RGBLIGHT_ANIMATIONS
-    #define RGBLIGHT_LIMIT_VAL 120
-    #define RGBLIGHT_HUE_STEP 10
-    #define RGBLIGHT_SAT_STEP 17
-    #define RGBLIGHT_VAL_STEP 17
-#endif
-
-#define OLED_FONT_H "keyboards/crkbd/lib/glcdfont.c"
+#ifdef OLED_ENABLE
+    #define OLED_FONT_H "keyboards/crkbd/lib/glcdfont.c"
+#endif // OLED_ENABLE
--- keyboards/crkbd/keymaps/jrodal98/rules.mk
+++ keyboards/crkbd/keymaps/jrodal98/rules.mk
@@ -1,14 +1,14 @@
-OLED_ENABLE = yes
-OLED_DRIVER = ssd1306
+# Default settings
 MOUSEKEY_ENABLE = no
-
-RGB_MATRIX_ENABLE = yes
 RGBLIGHT_ENABLE = no
-
-# letters are capitalized while active
-# automatically deactive after word ends
-# works with snakecase and backspaces
-# see https://docs.qmk.fm/#/feature_caps_word
 CAPS_WORD_ENABLE = yes
 
-# BOOTLOADER = atmel-dfu  # Elite-C
+ifeq ($(strip $(CONVERT_TO)), blok)
+    OLED_ENABLE = no
+    RGB_MATRIX_ENABLE = no
+else
+	OLED_ENABLE = yes
+	OLED_DRIVER = ssd1306
+	RGB_MATRIX_ENABLE = yes
+    BOOTLOADER = atmel-dfu # elite-c
+endif
